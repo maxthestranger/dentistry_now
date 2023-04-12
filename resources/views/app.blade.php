e4efbb8228ab8ced58a71b8cc18d344f0f9c10d2
maxthestranger <thestrangermax@gmail.com>
2023-04-12 19:18:30 +0300

    add initial setup

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Dentistry Now</title>
+
+    <!-- fonts -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+
+    <!-- styles -->
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+</body>
+</html>
